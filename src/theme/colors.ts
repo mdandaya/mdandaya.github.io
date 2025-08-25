51c257cec894bf05a27bae5c46959111c9766d97
Manuel D. Andaya <manuel@tuktu.ca>
2025-08-25 15:55:31 -0700

    styles, theme, scripts, tailwind init

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+/**
+ * Auto-generated file. Do not edit manually.
+ * Generated from styles/colors.css
+ */
+const colors = {
+  "primary": "#597675",
+  "primary2": "#79a57c",
+  "secondary": "#e3336b",
+  "tertiary": "#ff8800",
+  "blank": "#fffdfb",
+  "background": "#0a0a0a",
+  "background2": "#79a57c",
+  "text": "#597675",
+  "textContrast": "#fffdfb",
+  "foreground": "#ededed",
+  "colorBackground": "#0a0a0a",
+  "colorForeground": "#ededed",
+  "fontSans": "var(--font-geist-sans)",
+  "fontMono": "var(--font-geist-mono)"
+};
+
+export default colors;
